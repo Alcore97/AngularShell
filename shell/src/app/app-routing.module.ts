f8d5193eff305e2a618e0df4a18ea1f287978a41
Aleix <aleix.costa2@gmail.com>
2021-11-23 13:36:59 +0100

    first commit

---
+++ shell/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const URL = 'http://localhost:4000/remoteEntry.js';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'flights',
+    loadChildren: () => loadRemoteModule({
+      remoteEntry: URL,
+      remoteName: "mfe1",
+      exposedModule: './Module'
+    })
+    .then((m) => m.FlightsModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
